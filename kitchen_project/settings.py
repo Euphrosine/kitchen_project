7800f02b35a3670e6de8fb6fece93229125951a9
Euphrosine <euphrosine46@.com>
2023-11-11 00:57:03 -0800

    whitenoise package installed

--- kitchen_project/settings.py
+++ kitchen_project/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'kitchen_project.urls'
